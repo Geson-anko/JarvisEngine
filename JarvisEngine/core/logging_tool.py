15040a3671038f3f4fc31305a58df0c3a548c440
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-04-19 09:21:23 +0900

    ADD LoggingServer and Logger

---
+++ JarvisEngine/core/logging_tool.py
@@ -0,0 +1,13 @@
+"""
+This module provides useful tools for logging.
+
+- LoggingServer
+    Build a thread that manages log messages on the multiprocessing.
+    The log messages are sent from SocketLogger.
+
+- Logger
+    SocketLogger, send log to LoggingServer.
+    The name is changed to `Logger` in consideration of the case where
+    SocketLogger will no longer be used in the future.
+"""
+from logging_server import LoggingServer, SocketLogger as Logger
