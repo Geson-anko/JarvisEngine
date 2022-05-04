03d3861bd729899bb0569496c196c87351a285ab
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-04 17:12:39 +0900

    ADD test_clean

--- tests/core/test_name.py
+++ tests/core/test_name.py
@@ -2,3 +2,9 @@ from JarvisEngine.core import name
 
 def test_SEP():
     assert name.SEP == "."
+
+def test_clean():
+    assert name.clean("a.b.c") == "a.b.c"
+    assert name.clean("d.e.") == "d.e"
+    assert name.clean(".f.") == "f"
+    assert name.clean(".g.h") == "g.h"
