79cbbd25770cbf05f6e6566f11cd08120034daf7
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-04 17:12:33 +0900

    ADD clean method

--- JarvisEngine/core/name.py
+++ JarvisEngine/core/name.py
@@ -1,1 +1,16 @@
 SEP = "."
+
+def clean(name:str) -> str:
+    """clean name."""
+    if name[0] == SEP:
+        start = 1
+    else:
+        start = 0
+    
+    if name[-1] == SEP:
+        end = -1
+    else:
+        end = len(name)
+
+    return name[start:end]
+    
