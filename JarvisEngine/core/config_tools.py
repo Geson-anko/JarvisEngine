3388a99bb21ac8a9e9020e2e65f3828c86a71d10
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-05 13:57:08 +0900

    ADD read_json

--- JarvisEngine/core/config_tools.py
+++ JarvisEngine/core/config_tools.py
@@ -1,6 +1,13 @@
 import toml
+import json5
 
 def read_toml(file_path:str, mode:str="r", encoding:str="utf-8",**kwds) -> dict:
     """read toml file."""
     with open(file_path,mode,encoding=encoding,**kwds) as f:
         return toml.load(f)
+
+def read_json(file_path:str, mode:str="r", encoding:str="utf-8",**kwds) -> dict:
+    """read json and json5 files."""
+    with open(file_path,mode, encoding=encoding,**kwds) as f:
+        return json5.load(f)
+        
