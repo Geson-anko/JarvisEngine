599abc4b55a25fa3799e3b7c4d77d4edc16d1a0b
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-05 13:48:15 +0900

    ADD test_read_toml

--- tests/core/test_config_tools.py
+++ tests/core/test_config_tools.py
@@ -1,1 +1,11 @@
 from JarvisEngine.core import config_tools
+
+def test_read_toml():
+    conf = config_tools.read_toml("tests/core/config.toml")
+    assert conf["title"] == "TOML Example"
+    
+    database = conf["database"]
+    assert database["server"] == "192.168.1.1"
+    assert database["ports"] == [ 8000, 8001, 8002 ]
+    assert database["connection_max"] == 5000
+    assert database["enabled"] == True
