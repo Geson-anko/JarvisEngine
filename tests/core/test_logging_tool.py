6ee9cdcb20ea0c63596425ef6d2e5f786e38a432
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-06 11:30:26 +0900

    ADD test_getLogger

--- tests/core/test_logging_tool.py
+++ tests/core/test_logging_tool.py
@@ -3,3 +3,7 @@ from JarvisEngine.core import logging_tool
 def test_MAIN_LOGGER_NAME():
     assert logging_tool.MAIN_LOGGER_NAME == "MAIN"
     
+def test_getLogger():
+    import logging
+    assert isinstance(logging_tool.getLogger(), logging.Logger)
+    
