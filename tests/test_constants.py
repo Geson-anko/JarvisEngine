9ecd37aaf6f9d4f9e4499b3df75a02159452f450
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-09 10:56:38 +0900

    FIX test_DEFAULT_CONFIG_FILE

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -7,7 +7,7 @@ def test_ENGINE_PATH():
 
 def test_DEFAULT_CONFIG_FILE():
     import os
-    assert os.path.join(os.getcwd(), "JarvisEngine/default_engine_config.toml") == constants.DEFAULT_ENGINE_CONFIG_FILE
+    assert os.path.join(os.getcwd(), "JarvisEngine","default_engine_config.toml") == constants.DEFAULT_ENGINE_CONFIG_FILE
     assert os.path.isfile(constants.DEFAULT_ENGINE_CONFIG_FILE)
 
 def test_DEFAULT_CONFIG_FILE_NAME():
