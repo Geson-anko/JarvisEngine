5c6d98e08538f88d570137b1665e4ca1e4e3dca4
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-10 16:07:03 +0900

    ADD test_to_project_config

--- tests/apps/test_launcher.py
+++ tests/apps/test_launcher.py
@@ -1,1 +1,15 @@
 from JarvisEngine.apps import launcher
+from attr_dict import AttrDict
+import importlib
+
+def test_to_project_config():
+    config = AttrDict()
+    proj_conf = launcher.to_project_config(config)
+    assert "MAIN" in proj_conf
+    launcher_conf = proj_conf.MAIN
+    assert launcher_conf.path == "JarvisEngine.apps.Launcher"
+    mod, app = launcher_conf.path.rsplit(".",1)
+    assert getattr(importlib.import_module(mod), app) == launcher.Launcher
+    assert launcher_conf.thread == True
+    assert isinstance(launcher_conf.apps, AttrDict)
+    assert launcher_conf.apps == config
