2fbdee758a673091136ffffdf6c4a9154ade74f9
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-10 16:06:35 +0900

    ADD to_project_config

--- JarvisEngine/apps/launcher.py
+++ JarvisEngine/apps/launcher.py
@@ -1,10 +1,35 @@
 from .base_app import BaseApp, AttrDict
 from typing import *
 import os
+from ..core import logging_tool
+
+def to_project_config(config:AttrDict) -> AttrDict:
+    """convert `config` to `project_config`
+    `project_config` has the following structure.
+    ```
+    {
+        MAIN: {
+            path: "JarvisEngine.apps.Launcher",
+            thread: true,
+            apps: config
+        }
+
+    }
+    ```
+    """
+    pconf_dict = {
+        logging_tool.MAIN_LOGGER_NAME:{
+            "path": "JarvisEngine.apps.Launcher",
+            "thread": True,
+            "apps": config
+        }
+    }
+    project_config = AttrDict(pconf_dict)
+    return project_config
 
 class Launcher(BaseApp):
     """The origin of appcation processes.
-    
+
     """
 
     def __init__(
@@ -12,4 +37,3 @@ class Launcher(BaseApp):
         engine_config: AttrDict, project_config:AttrDict
     ) -> None:
         super().__init__(name, config, engine_config, project_config, os.path.dirname(__file__))
-
