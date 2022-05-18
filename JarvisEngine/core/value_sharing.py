dd161b8744a8e921e835160036a8175cf0b43435
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-18 17:13:12 +0900

    Update value_sharing.py
    ADD FolderDict_withLock object

--- JarvisEngine/core/value_sharing.py
+++ JarvisEngine/core/value_sharing.py
@@ -1,6 +1,8 @@
 import multiprocessing as mp
 from multiprocessing.sharedctypes import Synchronized, SynchronizedArray,SynchronizedString
 from typing import *
+from folder_dict import FolderDict
+import threading
 
 class ReadOnlyError(Exception): pass 
 
@@ -97,3 +99,32 @@ def make_readonly(
     else:
         raise ValueError(f"Unknown type {type(value)}. "
         "Please Synchronized, SynchronizedArray or SynchronizedString.")
+
+class FolderDict_withLock(FolderDict):
+    """
+    Protects FolderDict with Lock to prevent multiple threads 
+    from reading and writing at the same time
+    """
+    def __init__(
+        self, data = None, deep_copy: bool = False, *, sep: str = "/",
+        lock = None,
+        ) -> None:
+        super().__init__(data, deep_copy, sep=sep)
+        
+        
+        if lock is None:
+            self._lock = threading.RLock()
+        else:
+            self._lock = lock
+        
+    def get_lock(self) -> Union[threading._CRLock, Any]:
+        """returns lock object."""
+        return self._lock
+
+    def __getitem__(self, path: Union[str, Iterable[str]]) -> Union[Any, List[str]]:
+        with self._lock:
+            return super().__getitem__(path)
+
+    def  __setitem__(self, path: Union[str, Iterable[str]], value: Union[Any, Iterable[Any]]) -> None:
+        with self._lock:
+            return super().__setitem__(path, value)
