cae43d4839dff6d764667354b76bde8ee0c753eb
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-20 17:00:45 +0900

    Update test_base_app_override_methods.py
    ADD test_RegisterProcessSharedValues

--- tests/apps/test_base_app_override_methods.py
+++ tests/apps/test_base_app_override_methods.py
@@ -3,12 +3,14 @@ from JarvisEngine.apps import base_app
 # prepare
 import time
 from JarvisEngine.core.logging_tool import getLoggingServer
+from JarvisEngine.core.value_sharing import FolderDict_withLock
 from .test_base_app import (
     project_config, engine_config,PROJECT_DIR
 )
 from logging import INFO
 import os
 import copy
+import multiprocessing as mp
 
 engine_config = copy.deepcopy(engine_config)
 engine_config.logging.port = 20222
@@ -34,3 +36,20 @@ def test_Init(caplog):
         assert ("MAIN.App1",INFO, "Init1") in rec_tup
         assert ("MAIN.App1.App1_1",INFO, "Init1_1") in rec_tup
         assert ("MAIN.App1.App1_2",INFO, "Init1_2") in rec_tup
+
+def test_RegisterProcessSharedValues():
+    name = "MAIN"
+    config = project_config.MAIN
+    app_dir = PROJECT_DIR
+    with mp.Manager() as shmm, cd_project_dir():
+        MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
+        fdwl= FolderDict_withLock(sep=".")
+        MainApp.set_process_shared_values_to_all_apps(fdwl)
+        MainApp.RegisterProcessSharedValues(shmm)
+
+        assert fdwl["MAIN.App1.int_value"] == 100
+        assert fdwl["MAIN.App1.App1_2.float_value"] == 0.0
+        assert fdwl["MAIN.App1.App1_1.str_value"] == "apple"
+        assert fdwl["MAIN.App0.bool_value"] == True
+
+    
