5c555abcbb28f8fe6c4aaf63468ea8ce4d4bc32a
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-20 19:52:00 +0900

    Update test_launcher.py
    ADD test__init__

--- tests/apps/test_launcher.py
+++ tests/apps/test_launcher.py
@@ -1,6 +1,14 @@
 from JarvisEngine.apps import launcher
 from attr_dict import AttrDict
 import importlib
+from .test_base_app import (
+    TEST_CONFIG_FILE_PATH,_cd_project_dir,engine_config
+)
+from JarvisEngine.core.config_tools import dict2attr, read_json
+from JarvisEngine.core import logging_tool
+import os
+config = dict2attr(read_json(TEST_CONFIG_FILE_PATH))
+project_config = launcher.to_project_config(config)
 
 def test_to_project_config():
     config = AttrDict()
@@ -13,3 +21,11 @@ def test_to_project_config():
     assert launcher_conf.thread == True
     assert isinstance(launcher_conf.apps, AttrDict)
     assert launcher_conf.apps == config
+
+@_cd_project_dir
+def test__init__():
+    lnchr = launcher.Launcher(config, engine_config,os.getcwd())
+    assert lnchr.name == logging_tool.MAIN_LOGGER_NAME
+    assert lnchr.config == project_config[logging_tool.MAIN_LOGGER_NAME]
+    assert lnchr.project_config == project_config
+    assert lnchr.app_dir == os.getcwd()
