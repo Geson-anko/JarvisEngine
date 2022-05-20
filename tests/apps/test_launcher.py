5ef690de7b191262146bf0077fa14fb09a464764
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-20 20:43:00 +0900

    Update test_launcher.py
    ADD test_prepare_for_launching

--- tests/apps/test_launcher.py
+++ tests/apps/test_launcher.py
@@ -7,6 +7,8 @@ from .test_base_app import (
 from JarvisEngine.core.config_tools import dict2attr, read_json
 from JarvisEngine.core import logging_tool
 import os
+import multiprocessing as mp
+
 config = dict2attr(read_json(TEST_CONFIG_FILE_PATH))
 project_config = launcher.to_project_config(config)
 
@@ -29,3 +31,20 @@ def test__init__():
     assert lnchr.config == project_config[logging_tool.MAIN_LOGGER_NAME]
     assert lnchr.project_config == project_config
     assert lnchr.app_dir == os.getcwd()
+
+@_cd_project_dir
+def test_prepare_for_launching():
+    lnchr = launcher.Launcher(config, engine_config,os.getcwd())
+    with mp.Manager() as sync_manager:
+        p_sv = lnchr.prepare_for_launching(sync_manager)
+
+        assert lnchr.process_shared_values is None
+        for app in lnchr.child_apps.values():
+            assert app.process_shared_values is None
+            for app2 in app.child_apps.values():
+                assert app2.process_shared_values is None
+
+        assert p_sv["MAIN.App1.int_value"] == 100
+        assert p_sv["MAIN.App1.App1_2.float_value"] == 0.0
+        assert p_sv["MAIN.App1.App1_1.str_value"] == "apple"
+        assert p_sv["MAIN.App0.bool_value"] == True
