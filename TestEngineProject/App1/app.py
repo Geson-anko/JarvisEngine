e72b917fe7160c4e44cc62fa072cfbddf522f750
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-20 17:00:17 +0900

    UPDATE all app.py in TestEngineProject
    ADD override method `RegisterProcessSharedValues`

--- TestEngineProject/App1/app.py
+++ TestEngineProject/App1/app.py
@@ -5,3 +5,8 @@ class App1(BaseApp):
     def Init(self) -> None:
         self.logger.info("Init1")
         return super().Init()
+
+    def RegisterProcessSharedValues(self, sync_manager) -> None:
+        super().RegisterProcessSharedValues(sync_manager)
+
+        self.addProcessSharedValue("int_value",100)
