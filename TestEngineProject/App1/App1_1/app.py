42013026cdcceba4466cb2db09ee7b85909d86ea
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-22 11:38:13 +0900

    UPDATE all app.py in TestEngineProject
    ADD Awake method.

--- TestEngineProject/App1/App1_1/app.py
+++ TestEngineProject/App1/App1_1/app.py
@@ -13,3 +13,7 @@ class App1_1(BaseApp):
     def RegisterThreadSharedValues(self) -> None:
         super().RegisterThreadSharedValues()
         self.addThreadSharedValue("tuple_obj",(True,False))
+
+    def Awake(self) -> None:
+        self.logger.info("Awake")
+        assert self.process_shared_values == None
--- TestEngineProject/App1/app.py
+++ TestEngineProject/App1/app.py
@@ -14,3 +14,7 @@ class App1(BaseApp):
     def RegisterThreadSharedValues(self) -> None:
         super().RegisterThreadSharedValues()
         self.addThreadSharedValue("range_obj",range(10))
+
+    def Awake(self) -> None:
+        self.logger.info("Awake")
+        assert self.process_shared_values == None
