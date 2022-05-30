610ff3bb9762ae2c9241abf714098c2c08226c11
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-30 21:09:48 +0900

    UPDATE all app.py in TestEngineProject
    Rename `MAIN` to `Launcher`

--- TestEngineProject/App1/app.py
+++ TestEngineProject/App1/app.py
@@ -23,20 +23,20 @@ class App1(BaseApp):
 
     def Start(self) -> None:
         self.logger.info("Start")
-        assert self.getProcessSharedValue("MAIN.App0.bool_value") == True
-        assert self.getProcessSharedValue("MAIN.App1.int_value") == 100
-        assert self.getProcessSharedValue("MAIN.App1.App1_1.str_value") == "apple"
-        assert self.getProcessSharedValue("MAIN.App1.App1_2.float_value") == 0.0
+        assert self.getProcessSharedValue("Launcher.App0.bool_value") == True
+        assert self.getProcessSharedValue("Launcher.App1.int_value") == 100
+        assert self.getProcessSharedValue("Launcher.App1.App1_1.str_value") == "apple"
+        assert self.getProcessSharedValue("Launcher.App1.App1_2.float_value") == 0.0
         
-        assert self.getProcessSharedValue("MAIN.App0.shared_int").value == 0
-        assert self.getProcessSharedValue("MAIN.App1.shared_float").value == -10.0
-        assert self.getProcessSharedValue("MAIN.App1.App1_1.shared_bool").value == True
-        assert self.getProcessSharedValue("MAIN.App1.App1_2.shared_str").value == b"abc"
+        assert self.getProcessSharedValue("Launcher.App0.shared_int").value == 0
+        assert self.getProcessSharedValue("Launcher.App1.shared_float").value == -10.0
+        assert self.getProcessSharedValue("Launcher.App1.App1_1.shared_bool").value == True
+        assert self.getProcessSharedValue("Launcher.App1.App1_2.shared_str").value == b"abc"
 
-        assert self.getThreadSharedValue("MAIN.App0.set_obj") is None
-        assert self.getThreadSharedValue("MAIN.App1.range_obj") == range(10)
-        assert self.getThreadSharedValue("MAIN.App1.App1_1.tuple_obj") == (True, False)
-        assert self.getThreadSharedValue("MAIN.App1.App1_2.list_obj") is None
+        assert self.getThreadSharedValue("Launcher.App0.set_obj") is None
+        assert self.getThreadSharedValue("Launcher.App1.range_obj") == range(10)
+        assert self.getThreadSharedValue("Launcher.App1.App1_1.tuple_obj") == (True, False)
+        assert self.getThreadSharedValue("Launcher.App1.App1_2.list_obj") is None
 
     frame_rate = 5
     def Update(self, delta_time: float) -> None:
