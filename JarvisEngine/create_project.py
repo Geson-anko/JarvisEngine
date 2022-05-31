54041d948109cd504003981fa4884d0743fda468
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-05-31 19:42:29 +0900

    Update create_project.py
    ADD constants, TEMPLATE_PROJECT_PATH,
    TEMPLATE_CONFIG_FILE_PATH ,
    TEMPLATE_APP_FILE_PATH

--- JarvisEngine/create_project.py
+++ JarvisEngine/create_project.py
@@ -1,4 +1,16 @@
+import os
 from .core import parsers
+from .constants import ENGINE_PATH,DEFAULT_CONFIG_FILE_NAME
+
+# The absolute path to the template project
+TEMPLATE_PROJECT_PATH = os.path.join(ENGINE_PATH, "template_project")
+
+# The absolute path to the template config file.
+TEMPLATE_CONFIG_FILE_PATH = os.path.join(TEMPLATE_PROJECT_PATH, DEFAULT_CONFIG_FILE_NAME)
+
+# The absolute path to the template application file
+TEMPLATE_APP_FILE_PATH = os.path.join(TEMPLATE_PROJECT_PATH,"app.py")
+
 
 def create():
     """create JE project."""
