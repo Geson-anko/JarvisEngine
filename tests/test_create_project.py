78f5db8565d71e766e6a764ca8274e877da0cc6b
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-06-01 17:04:42 +0900

    Update test_create_project.py
    ADD test_TEMPLATE_APP_FILE_NAME

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -1,7 +1,8 @@
 import os
 from JarvisEngine import create_project
 from JarvisEngine.create_project import (
-    TEMPLATE_PROJECT_PATH, TEMPLATE_CONFIG_FILE_PATH, TEMPLATE_APP_FILE_PATH
+    TEMPLATE_PROJECT_PATH, TEMPLATE_CONFIG_FILE_PATH, TEMPLATE_APP_FILE_PATH,
+    TEMPLATE_APP_FILE_NAME
 )
 from JarvisEngine.constants import DEFAULT_CONFIG_FILE_NAME
 
@@ -11,15 +12,22 @@ def test_TEMPLATE_PROJECt_PATH():
         os.getcwd(),"JarvisEngine", "template_project"
     )
 
+
+def test_TEMPLATE_APP_FILE_NAME():
+    assert TEMPLATE_APP_FILE_NAME == "app.py"
+
 def test_TEMPLATE_APP_FILE_PATH():
     assert os.path.isfile(TEMPLATE_APP_FILE_PATH)
-    assert TEMPLATE_APP_FILE_PATH == os.path.join(TEMPLATE_PROJECT_PATH, "app.py")
+    assert TEMPLATE_APP_FILE_PATH == os.path.join(
+        TEMPLATE_PROJECT_PATH, TEMPLATE_APP_FILE_NAME
+    )
 
 def test_TEMPLATE_CONFIG_FILE_PATH():
     assert os.path.isfile(TEMPLATE_CONFIG_FILE_PATH)
     assert TEMPLATE_CONFIG_FILE_PATH == os.path.join(
         TEMPLATE_PROJECT_PATH, DEFAULT_CONFIG_FILE_NAME
     )
+
 
 def test_make_project_folder():
     creating_dir = "_test_project"
