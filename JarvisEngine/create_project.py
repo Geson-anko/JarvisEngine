0a83d01ca0959629fc070a6871699291514b5a17
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-06-01 16:56:17 +0900

    Update create_project.py
    ADD make_project_folder

--- JarvisEngine/create_project.py
+++ JarvisEngine/create_project.py
@@ -1,5 +1,5 @@
 import os
-from .core import parsers
+from .core import parsers, logging_tool
 from .constants import ENGINE_PATH,DEFAULT_CONFIG_FILE_NAME
 
 # The absolute path to the template project
@@ -14,5 +14,11 @@ TEMPLATE_APP_FILE_PATH = os.path.join(TEMPLATE_PROJECT_PATH,"app.py")
 
 def create():
     """create JE project."""
-    print("create_project")
     args = parsers.at_creating()
+
+def make_project_folder(creating_dir:str) -> None:
+    """If creating_dir does not exist, make it to disk."""
+    if os.path.isdir(creating_dir):
+        return
+    else:
+        os.makedirs(creating_dir)
