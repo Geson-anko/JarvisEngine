4399d4015092ea53a31390572f904f3419222ce5
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-06-01 17:15:16 +0900

    Update test_create_project.py
    ADD test_copy_files

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from JarvisEngine import create_project
 from JarvisEngine.create_project import (
     TEMPLATE_PROJECT_PATH, TEMPLATE_CONFIG_FILE_PATH, TEMPLATE_APP_FILE_PATH,
@@ -37,3 +38,14 @@ def test_make_project_folder():
 
     os.rmdir(creating_dir)
     assert not os.path.exists(creating_dir) # remove after test.
+
+def test_copy_files():
+    creating_dir = "_test_project"
+    create_project.make_project_folder(creating_dir)
+    create_project.copy_files(creating_dir)
+
+    assert os.path.isfile(os.path.join(creating_dir,DEFAULT_CONFIG_FILE_NAME))
+    assert os.path.isfile(os.path.join(creating_dir,TEMPLATE_APP_FILE_NAME))
+    
+    shutil.rmtree(creating_dir)
+    assert not os.path.exists(creating_dir)
