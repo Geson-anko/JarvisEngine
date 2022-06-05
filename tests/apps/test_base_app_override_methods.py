e4011dce521b6dcf3e2f8cc097096108d2da16c7
Subaru8556 <tkmkn6100@icloud.com>
2022-06-05 18:20:08 +0900

    test_base_app_override_methods.py

--- tests/apps/test_base_app_override_methods.py
+++ tests/apps/test_base_app_override_methods.py
@@ -1,49 +1,52 @@
-from JarvisEngine.apps import base_app
+import copy
+import multiprocessing as mp
+import os
 
 # prepare
 import time
+from logging import INFO
+
+from JarvisEngine.apps import base_app
 from JarvisEngine.core.logging_tool import getLoggingServer
 from JarvisEngine.core.value_sharing import FolderDict_withLock
-from .test_base_app import (
-    project_config, engine_config,PROJECT_DIR
-)
-from logging import INFO
-import os
-import copy
-import multiprocessing as mp
+
+from .test_base_app import PROJECT_DIR, engine_config, project_config
 
 engine_config = copy.deepcopy(engine_config)
 engine_config.logging.port = 20222
 ls = getLoggingServer(engine_config.logging)
 ls.start()
 
+
 class cd_project_dir:
     def __enter__(self):
         os.chdir(PROJECT_DIR)
+
     def __exit__(self, *args):
         os.chdir("..")
 
-        
+
 def test_Init(caplog):
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
     with cd_project_dir():
-        MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
+        MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
         time.sleep(0.1)
         rec_tup = caplog.record_tuples
-        assert ("Launcher.App0",INFO, "Init0") in rec_tup
-        assert ("Launcher.App1",INFO, "Init1") in rec_tup
-        assert ("Launcher.App1.App1_1",INFO, "Init1_1") in rec_tup
-        assert ("Launcher.App1.App1_2",INFO, "Init1_2") in rec_tup
+        assert ("Launcher.App0", INFO, "Init0") in rec_tup
+        assert ("Launcher.App1", INFO, "Init1") in rec_tup
+        assert ("Launcher.App1.App1_1", INFO, "Init1_1") in rec_tup
+        assert ("Launcher.App1.App1_2", INFO, "Init1_2") in rec_tup
+
 
 def test_RegisterProcessSharedValues():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
     with mp.Manager() as shmm, cd_project_dir():
-        MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-        fdwl= FolderDict_withLock(sep=".")
+        MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+        fdwl = FolderDict_withLock(sep=".")
         MainApp.set_process_shared_values_to_all_apps(fdwl)
         MainApp.RegisterProcessSharedValues(shmm)
 
@@ -52,13 +55,14 @@ def test_RegisterProcessSharedValues():
         assert fdwl["Launcher.App1.App1_1.str_value"] == "apple"
         assert fdwl["Launcher.App0.bool_value"] == True
 
+
 def test_RegisterThreadSharedValues():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
     with cd_project_dir():
-        MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-        fdwl= FolderDict_withLock(sep=".")
+        MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+        fdwl = FolderDict_withLock(sep=".")
         MainApp.set_thread_shared_values_to_all_apps(fdwl)
         MainApp.RegisterThreadSharedValues()
 
@@ -74,4 +78,3 @@ def test_RegisterThreadSharedValues():
         assert fdwl["Launcher.App1.range_obj"] == range(10)
         assert fdwl["Launcher.App1.App1_1.tuple_obj"] == (True, False)
         assert fdwl["Launcher.App1.App1_2.list_obj"] is None
-        
