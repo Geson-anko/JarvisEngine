77a25a257f3b89aec487ba66bebdb2d8863487c7
Subaru8556 <tkmkn6100@icloud.com>
2022-06-05 17:53:30 +0900

    fix app.py

--- TestEngineProject/App0/app.py
+++ TestEngineProject/App0/app.py
@@ -1,14 +1,16 @@
-from JarvisEngine.apps import BaseApp
 import multiprocessing as mp
+
+from JarvisEngine.apps import BaseApp
+
+
 class App0(BaseApp):
-    
     def Init(self) -> None:
         self.logger.info("Init0")
 
     def RegisterProcessSharedValues(self, sync_manager) -> None:
         super().RegisterProcessSharedValues(sync_manager)
-        self.addProcessSharedValue("bool_value",True)
-        self.addProcessSharedValue("shared_int",mp.Value("i",0))
+        self.addProcessSharedValue("bool_value", True)
+        self.addProcessSharedValue("shared_int", mp.Value("i", 0))
 
     def RegisterThreadSharedValues(self) -> None:
         super().RegisterThreadSharedValues()
@@ -16,18 +18,18 @@ class App0(BaseApp):
 
     def Awake(self) -> None:
         self.logger.info("Awake")
-        assert self.process_shared_values == None
+        assert self.process_shared_values is None
 
     def Start(self) -> None:
         self.logger.info("Start")
-        assert self.getProcessSharedValue("Launcher.App0.bool_value") == True
+        assert self.getProcessSharedValue("Launcher.App0.bool_value") is True
         assert self.getProcessSharedValue("Launcher.App1.int_value") == 100
         assert self.getProcessSharedValue("Launcher.App1.App1_1.str_value") == "apple"
         assert self.getProcessSharedValue("Launcher.App1.App1_2.float_value") == 0.0
 
         assert self.getProcessSharedValue("Launcher.App0.shared_int").value == 0
         assert self.getProcessSharedValue("Launcher.App1.shared_float").value == -10.0
-        assert self.getProcessSharedValue("Launcher.App1.App1_1.shared_bool").value == True
+        assert self.getProcessSharedValue("Launcher.App1.App1_1.shared_bool").value is True
         assert self.getProcessSharedValue("Launcher.App1.App1_2.shared_str").value == b"abc"
 
         assert self.getThreadSharedValue("Launcher.App0.set_obj") == {"number"}
@@ -36,6 +38,7 @@ class App0(BaseApp):
         assert self.getThreadSharedValue("Launcher.App1.App1_2.list_obj") is None
 
     frame_rate = 0.0
+
     def Update(self, delta_time: float) -> None:
         self.logger.info("Update")
 
