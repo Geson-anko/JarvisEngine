0a6805ba81b2463d340493c4afd146d1f44818c1
Subaru8556 <tkmkn6100@icloud.com>
2022-06-05 18:17:05 +0900

    fix test_base_app_launcher.py

--- tests/apps/test_base_app_launch.py
+++ tests/apps/test_base_app_launch.py
@@ -1,41 +1,44 @@
-from JarvisEngine.apps.base_app import BaseApp
-
-# prepare 
-import time
-from JarvisEngine.run_project import create_shutdown
-from JarvisEngine.core.logging_tool import getLoggingServer
-from JarvisEngine.core.value_sharing import FolderDict_withLock
-from JarvisEngine.apps.launcher import Launcher
-from JarvisEngine.constants import SHUTDOWN_NAME
-from .test_base_app import (
-    project_config, engine_config,PROJECT_DIR
-)
-from logging import DEBUG, INFO, WARNING
-import os
 import copy
 import multiprocessing as mp
+import os
 import threading
+
+# prepare
+import time
+from logging import DEBUG, INFO, WARNING
+
+from JarvisEngine.apps.base_app import BaseApp
+from JarvisEngine.apps.launcher import Launcher
+from JarvisEngine.constants import SHUTDOWN_NAME
+from JarvisEngine.core.logging_tool import getLoggingServer
+from JarvisEngine.core.value_sharing import FolderDict_withLock
+from JarvisEngine.run_project import create_shutdown
+
+from .test_base_app import PROJECT_DIR, engine_config, project_config
 
 engine_config = copy.deepcopy(engine_config)
 engine_config.logging.port = 20223
 ls = getLoggingServer(engine_config.logging)
 ls.start()
 
+
 class cd_project_dir:
     def __enter__(self):
         os.chdir(PROJECT_DIR)
+
     def __exit__(self, *args):
         os.chdir("..")
+
 
 def test_launch(caplog):
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
     with cd_project_dir(), mp.Manager() as sync_manager:
-        #MainApp = BaseApp(name, config, engine_config, project_config,app_dir)        
-        #p_sv = Launcher.prepare_for_launching(MainApp, sync_manager)
-        #MainApp.launch(p_sv)
-        LauncherApp = Launcher(config.apps,engine_config,app_dir)
+        # MainApp = BaseApp(name, config, engine_config, project_config,app_dir)
+        # p_sv = Launcher.prepare_for_launching(MainApp, sync_manager)
+        # MainApp.launch(p_sv)
+        LauncherApp = Launcher(config.apps, engine_config, app_dir)
         p_sv = LauncherApp.prepare_for_launching(sync_manager)
         shutdown = create_shutdown(p_sv)
         LauncherApp.launch(p_sv)
@@ -51,14 +54,14 @@ def test_launch(caplog):
         for p in processes:
             assert isinstance(p, mp.Process)
             assert p.is_alive()
-        
+
         assert threads[0].name == "Launcher.App0"
-        assert processes[0].name == "Launcher.App1"        
+        assert processes[0].name == "Launcher.App1"
 
         time.sleep(0.9)
         shutdown.value = True
         LauncherApp.join()
-        
+
         # join_child_apps
         for t in threads:
             assert not t.is_alive()
@@ -75,7 +78,7 @@ def test_launch(caplog):
         assert ("Launcher.App1", INFO, "launch") in rec_tup
         assert ("Launcher.App1.App1_1", INFO, "launch") in rec_tup
         assert ("Launcher.App1.App1_2", INFO, "launch") in rec_tup
-        
+
         # Awake
         assert ("Launcher.App0", INFO, "Awake") in rec_tup
         assert ("Launcher.App1", INFO, "Awake") in rec_tup
@@ -102,7 +105,7 @@ def test_launch(caplog):
 
         ### updating in 5 fps.
         assert ("Launcher.App1", INFO, "Update") in rec_tup
-        assert 6 >= rec_tup.count(("Launcher.App1", INFO, "Update")) >= 4 
+        assert 6 >= rec_tup.count(("Launcher.App1", INFO, "Update")) >= 4
 
         ### updating in 10 fps.
         assert ("Launcher.App1.App1_1", INFO, "Update") in rec_tup
@@ -124,11 +127,11 @@ def test_launch(caplog):
         assert ("Launcher.App1.App1_2", INFO, "Terminate.") in rec_tup
 
         # terminate
-        assert("Launcher", DEBUG, "terminate") in rec_tup
-        assert("Launcher.App0", DEBUG, "terminate") in rec_tup
-        assert("Launcher.App1", DEBUG, "terminate") in rec_tup
-        assert("Launcher.App1.App1_1", DEBUG, "terminate") in rec_tup
-        assert("Launcher.App1.App1_2", DEBUG, "terminate") in rec_tup
+        assert ("Launcher", DEBUG, "terminate") in rec_tup
+        assert ("Launcher.App0", DEBUG, "terminate") in rec_tup
+        assert ("Launcher.App1", DEBUG, "terminate") in rec_tup
+        assert ("Launcher.App1.App1_1", DEBUG, "terminate") in rec_tup
+        assert ("Launcher.App1.App1_2", DEBUG, "terminate") in rec_tup
 
     for record in caplog.records:
         assert record.levelno < WARNING, record.message
