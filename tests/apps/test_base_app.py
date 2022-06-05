780c680db4b3905bf18fe3346da726f9664a7fec
Subaru8556 <tkmkn6100@icloud.com>
2022-06-05 18:09:58 +0900

    fix test_base_app.py

--- tests/apps/test_base_app.py
+++ tests/apps/test_base_app.py
@@ -1,19 +1,20 @@
-from JarvisEngine.apps import base_app
-
-# prepare 
-from JarvisEngine.core.config_tools import read_json, read_toml, dict2attr
-from JarvisEngine.constants import DEFAULT_ENGINE_CONFIG_FILE
-from JarvisEngine.core import logging_tool
-from JarvisEngine.apps.launcher import to_project_config
+import multiprocessing as mp
 import os
 import sys
-from JarvisEngine.core.value_sharing import FolderDict_withLock
-import multiprocessing as mp
 import timeit
 
+from JarvisEngine.apps import base_app
+from JarvisEngine.apps.launcher import to_project_config
+from JarvisEngine.constants import DEFAULT_ENGINE_CONFIG_FILE
+from JarvisEngine.core import logging_tool
+
+# prepare
+from JarvisEngine.core.config_tools import dict2attr, read_json, read_toml
+from JarvisEngine.core.value_sharing import FolderDict_withLock
+
 PROJECT_DIR = "TestEngineProject"
-sys.path.insert(0,os.path.join(os.getcwd(),PROJECT_DIR))
-TEST_CONFIG_FILE_PATH = os.path.join(PROJECT_DIR,"config.json5")
+sys.path.insert(0, os.path.join(os.getcwd(), PROJECT_DIR))
+TEST_CONFIG_FILE_PATH = os.path.join(PROJECT_DIR, "config.json5")
 
 project_config = dict2attr(read_json(TEST_CONFIG_FILE_PATH))
 project_config = to_project_config(project_config)
@@ -22,35 +23,41 @@ engine_config = read_toml(DEFAULT_ENGINE_CONFIG_FILE)
 engine_config["logging"]["log_level"] = "DEBUG"
 engine_config = dict2attr(engine_config)
 
+
 def _cd_project_dir(func):
     def cd(*args, **kwds):
         os.chdir(PROJECT_DIR)
-        func(*args,**kwds)
+        func(*args, **kwds)
         os.chdir("../")
+
     return cd
 
-def _check_property_override(app, attr:str):
+
+def _check_property_override(app, attr: str):
     try:
         setattr(app, attr, None)
         raise AssertionError(f"The property `{attr}` of {app} is overrided!")
-    except AttributeError: pass
+    except AttributeError:
+        pass
+
 
 @_cd_project_dir
 def test_import_app():
-    
+
     path = "App0.app.App0"
     app_cls, mod = base_app.BaseApp.import_app(path)
     assert issubclass(app_cls, base_app.BaseApp)
-    assert mod.__file__ == os.path.join(os.getcwd(), "App0","app.py")
+    assert mod.__file__ == os.path.join(os.getcwd(), "App0", "app.py")
     try:
         base_app.BaseApp.import_app("JarvisEngine.core.Logger")
         raise AssertionError
-    except ImportError: pass
-    
+    except ImportError:
+        pass
+
 
 @_cd_project_dir
 def test__init__():
-    
+
     # test_properties():
     name = "Launcher.App1.App1_1"
     config = project_config.Launcher.apps.App1
@@ -122,13 +129,14 @@ def test__init__():
     assert App1_1.app_dir == os.path.join(app_dir, "App1_1")
     assert App1_2.app_dir == os.path.join(app_dir, "App1_2")
 
+
 @_cd_project_dir
 def test_process_shared_values():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+
     # Initial value is None
     assert MainApp.process_shared_values == None
     App0 = MainApp.child_apps["App0"]
@@ -136,12 +144,12 @@ def test_process_shared_values():
     assert App0.process_shared_values == None
     assert App1.process_shared_values == None
 
-    fdwl= FolderDict_withLock(sep=".")
+    fdwl = FolderDict_withLock(sep=".")
 
-    MainApp.process_shared_values = fdwl 
+    MainApp.process_shared_values = fdwl
     assert MainApp.process_shared_values is fdwl
     # Not set to child_apps.
-    assert App0.process_shared_values == None 
+    assert App0.process_shared_values == None
     assert App1.process_shared_values == None
     App1_1 = App1.child_apps["App1_1"]
     App1_2 = App1.child_apps["App1_2"]
@@ -149,14 +157,15 @@ def test_process_shared_values():
     assert App1_1.process_shared_values == None
     assert App1_2.process_shared_values == None
 
+
 @_cd_project_dir
 def test_set_process_shared_values_to_all_apps():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    
-    fdwl= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+
+    fdwl = FolderDict_withLock(sep=".")
     MainApp.set_process_shared_values_to_all_apps(fdwl)
     assert MainApp.process_shared_values is fdwl
     App0 = MainApp.child_apps["App0"]
@@ -169,28 +178,29 @@ def test_set_process_shared_values_to_all_apps():
     assert App1_1.process_shared_values is fdwl
     assert App1_2.process_shared_values is fdwl
 
-@_cd_project_dir
-def test_thread_shared_values():
-    name = "Launcher"
-    config = project_config.Launcher
-    app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    
-    # Initial values is None
-    assert MainApp.thread_shared_values == None
-    fdwl= FolderDict_withLock(sep=".")
-    MainApp.thread_shared_values = fdwl
-    assert MainApp.thread_shared_values is fdwl
-    
 
 @_cd_project_dir
 def test_thread_shared_values():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    
-    fdwl= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+
+    # Initial values is None
+    assert MainApp.thread_shared_values == None
+    fdwl = FolderDict_withLock(sep=".")
+    MainApp.thread_shared_values = fdwl
+    assert MainApp.thread_shared_values is fdwl
+
+
+@_cd_project_dir
+def test_thread_shared_values():
+    name = "Launcher"
+    config = project_config.Launcher
+    app_dir = PROJECT_DIR
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+
+    fdwl = FolderDict_withLock(sep=".")
     MainApp.set_thread_shared_values_to_all_apps(fdwl)
     assert MainApp.thread_shared_values is fdwl
     App0 = MainApp.child_thread_apps["App0"]
@@ -200,22 +210,23 @@ def test_thread_shared_values():
 
     assert App0.thread_shared_values is fdwl
     assert App1.thread_shared_values is None
-    assert App1_1.thread_shared_values is None # App1 is not thread
+    assert App1_1.thread_shared_values is None  # App1 is not thread
     assert App1_2.thread_shared_values is None
 
     App1_1.set_thread_shared_values_to_all_apps(fdwl)
-    assert App1_1.thread_shared_values is fdwl 
+    assert App1_1.thread_shared_values is fdwl
     assert App1_2.thread_shared_values is None
+
 
 @_cd_project_dir
 def test__add_shared_value():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    
-    fdwl_thread= FolderDict_withLock(sep=".")
-    fdwl_process= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+
+    fdwl_thread = FolderDict_withLock(sep=".")
+    fdwl_process = FolderDict_withLock(sep=".")
     MainApp.set_process_shared_values_to_all_apps(fdwl_process)
     MainApp.set_thread_shared_values_to_all_apps(fdwl_thread)
 
@@ -224,53 +235,56 @@ def test__add_shared_value():
     assert MainApp.process_shared_values["Launcher.aaa"] is None
     assert fdwl_thread["Launcher.aaa"] == 10
 
-    MainApp._add_shared_value("bbb",20,for_thread=False)
+    MainApp._add_shared_value("bbb", 20, for_thread=False)
     assert MainApp.thread_shared_values["Launcher.bbb"] is None
     assert MainApp.process_shared_values["Launcher.bbb"] == 20
     assert fdwl_process["Launcher.bbb"] == 20
-    
+
+
 @_cd_project_dir
 def test_addProcessSharedValue():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    fdwl= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+    fdwl = FolderDict_withLock(sep=".")
     MainApp.process_shared_values = fdwl
 
-    MainApp.addProcessSharedValue("ccc",30)
-    MainApp.addProcessSharedValue("ddd",40)
-    MainApp.addProcessSharedValue("eee",50)
+    MainApp.addProcessSharedValue("ccc", 30)
+    MainApp.addProcessSharedValue("ddd", 40)
+    MainApp.addProcessSharedValue("eee", 50)
     assert MainApp.process_shared_values["Launcher.ccc"] == 30
     assert MainApp.process_shared_values["Launcher.ddd"] == 40
     assert MainApp.process_shared_values["Launcher.eee"] == 50
+
 
 @_cd_project_dir
 def test_addThreadSharedValue():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    fdwl= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+    fdwl = FolderDict_withLock(sep=".")
 
-    MainApp.thread_shared_values = fdwl 
+    MainApp.thread_shared_values = fdwl
 
-    MainApp.addThreadSharedValue("fff",60)
-    MainApp.addThreadSharedValue("ggg",70)
-    MainApp.addThreadSharedValue("hhh",80)
+    MainApp.addThreadSharedValue("fff", 60)
+    MainApp.addThreadSharedValue("ggg", 70)
+    MainApp.addThreadSharedValue("hhh", 80)
     assert MainApp.thread_shared_values["Launcher.fff"] == 60
     assert MainApp.thread_shared_values["Launcher.ggg"] == 70
     assert MainApp.thread_shared_values["Launcher.hhh"] == 80
+
 
 @_cd_project_dir
 def test__get_shared_value():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    
-    fdwl_thread= FolderDict_withLock(sep=".")
-    fdwl_process= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+
+    fdwl_thread = FolderDict_withLock(sep=".")
+    fdwl_process = FolderDict_withLock(sep=".")
     MainApp.set_process_shared_values_to_all_apps(fdwl_process)
     MainApp.set_thread_shared_values_to_all_apps(fdwl_thread)
 
@@ -279,41 +293,41 @@ def test__get_shared_value():
     App1_1 = App1.child_apps["App1_1"]
     App1_2 = App1.child_apps["App1_2"]
     # Process shared value
-    MainApp.addProcessSharedValue("aaa",10)
-    App0.addProcessSharedValue("bbb",True)
+    MainApp.addProcessSharedValue("aaa", 10)
+    App0.addProcessSharedValue("bbb", True)
     App1.addProcessSharedValue("ccc", "apple")
-    App1_1.addProcessSharedValue("ddd",1.0) 
+    App1_1.addProcessSharedValue("ddd", 1.0)
     # Thread shared value
-    MainApp.addThreadSharedValue("eee",False)
-    App0.addThreadSharedValue("fff",20)
+    MainApp.addThreadSharedValue("eee", False)
+    App0.addThreadSharedValue("fff", 20)
 
-    assert MainApp._get_shared_value("Launcher.aaa",False) == 10
-    assert MainApp._get_shared_value(".aaa",False) == 10
-    assert MainApp._get_shared_value("Launcher.App0.bbb",False) == True
+    assert MainApp._get_shared_value("Launcher.aaa", False) == 10
+    assert MainApp._get_shared_value(".aaa", False) == 10
+    assert MainApp._get_shared_value("Launcher.App0.bbb", False) == True
     assert App0._get_shared_value("..App1.ccc", False) == "apple"
-    assert App1._get_shared_value(".App1_1.ddd",False) == 1.0
-    
-    assert MainApp._get_shared_value("..Launcher.eee",True) == False
-    assert App0._get_shared_value("Launcher.App0.fff" ,True) == 20
+    assert App1._get_shared_value(".App1_1.ddd", False) == 1.0
+
+    assert MainApp._get_shared_value("..Launcher.eee", True) == False
+    assert App0._get_shared_value("Launcher.App0.fff", True) == 20
+
 
 @_cd_project_dir
 def test_getProcessSharedValue():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    fdwl_process= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+    fdwl_process = FolderDict_withLock(sep=".")
     MainApp.set_process_shared_values_to_all_apps(fdwl_process)
     App0 = MainApp.child_apps["App0"]
     App1 = MainApp.child_apps["App1"]
     App1_1 = App1.child_apps["App1_1"]
     App1_2 = App1.child_apps["App1_2"]
     # Process shared value
-    MainApp.addProcessSharedValue("aaa",10)
-    App0.addProcessSharedValue("bbb",True)
+    MainApp.addProcessSharedValue("aaa", 10)
+    App0.addProcessSharedValue("bbb", True)
     App1.addProcessSharedValue("ccc", "apple")
-    App1_1.addProcessSharedValue("ddd",1.0) 
-
+    App1_1.addProcessSharedValue("ddd", 1.0)
 
     assert MainApp.getProcessSharedValue("Launcher.aaa") == 10
     assert MainApp.getProcessSharedValue(".aaa") == 10
@@ -321,30 +335,32 @@ def test_getProcessSharedValue():
     assert App0.getProcessSharedValue("..App1.ccc") == "apple"
     assert App1.getProcessSharedValue(".App1_1.ddd") == 1.0
 
+
 @_cd_project_dir
 def test_getThreadSharedValue():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
-    fdwl_thread= FolderDict_withLock(sep=".")
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
+    fdwl_thread = FolderDict_withLock(sep=".")
     MainApp.set_thread_shared_values_to_all_apps(fdwl_thread)
 
     App0 = MainApp.child_apps["App0"]
 
     # Thread shared value
-    MainApp.addThreadSharedValue("eee",False)
-    App0.addThreadSharedValue("fff",20)
+    MainApp.addThreadSharedValue("eee", False)
+    App0.addThreadSharedValue("fff", 20)
 
     assert MainApp.getThreadSharedValue("..Launcher.eee") == False
     assert App0.getThreadSharedValue("Launcher.App0.fff") == 20
+
 
 @_cd_project_dir
 def test_prepare_for_launching_thread_apps():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
     MainApp.prepare_for_launching_thread_apps()
 
     t_sv = MainApp.thread_shared_values
@@ -359,16 +375,17 @@ def test_prepare_for_launching_thread_apps():
     assert t_sv["Launcher.App1.App1_1.tuple_obj"] == (True, False)
     assert t_sv["Launcher.App1.App1_2.list_obj"] is None
 
+
 @_cd_project_dir
 def test_adjust_frame_rate():
     name = "Launcher"
     config = project_config.Launcher
     app_dir = PROJECT_DIR
-    MainApp = base_app.BaseApp(name, config, engine_config,project_config,app_dir)
+    MainApp = base_app.BaseApp(name, config, engine_config, project_config, app_dir)
 
     assert MainApp.frame_rate == 0.0
     assert MainApp._update_start_time == float("-inf")
 
     MainApp.frame_rate = 10
     MainApp.adjust_update_frame_rate()
-    assert 9 < 1/timeit.timeit("MainApp.adjust_update_frame_rate()",globals=locals(), number=5) * 5 < 11
+    assert 9 < 1 / timeit.timeit("MainApp.adjust_update_frame_rate()", globals=locals(), number=5) * 5 < 11
