85c02ede2d0e710522909f7b3fc5936033f87e0f
Subaru8556 <tkmkn6100@icloud.com>
2022-06-05 18:53:32 +0900

    fix test_apps.py

--- tests/test_apps.py
+++ tests/test_apps.py
@@ -1,8 +1,11 @@
 from JarvisEngine import apps
 
+
 def test_import():
     from JarvisEngine.apps.base_app import BaseApp
+
     assert apps.BaseApp is BaseApp
 
     from JarvisEngine.apps.launcher import Launcher
+
     assert apps.Launcher == Launcher
