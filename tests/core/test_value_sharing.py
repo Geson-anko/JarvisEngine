510a8b80edce52141d599d5614991cce64318659
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-06-27 11:09:17 +0900

    Update test_value_sharing.py
    ADD test_import

---
+++ tests/core/test_value_sharing.py
@@ -0,0 +1,9 @@
+def test_import():
+    from JarvisEngine.core.value_sharing import (
+        FolderDictWithLock,
+        ReadOnly,
+        ReadOnlyArray,
+        ReadOnlyError,
+        ReadOnlyString,
+        ReadOnlyValue,
+    )
