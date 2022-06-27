6a5d4361db07c81aa4fad69a1a70edc61f290c8d
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-06-27 10:20:29 +0900

    Create test_folder_dict_with_lock.py
    MOVE test of FolderDictWithLock of test_value_sharing.py to this.

---
+++ tests/core/value_sharing/test_folder_dict_with_lock.py
@@ -0,0 +1,18 @@
+import threading
+
+from JarvisEngine.core.value_sharing import folder_dict_with_lock
+
+
+def test_FolderDict_withLock():
+    fdwl = folder_dict_with_lock.FolderDictWithLock(sep=".")
+    rlock_type = type(threading.RLock())
+    assert isinstance(fdwl._lock, rlock_type)
+    assert fdwl._lock == fdwl.get_lock()
+
+    lock = threading.RLock()
+    fdwl = folder_dict_with_lock.FolderDictWithLock(sep=".", lock=lock)
+    assert fdwl.get_lock() == lock
+
+    # Not implemented locking test because I don't know how to test it.
+    fdwl["a.b.c"] = 1
+    fdwl["a.b.c"]
